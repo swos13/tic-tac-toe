468428287cbee11081961bf89d32fd0b1ab9c638
swos <szymon.wos1319@gmail.com>
2024-01-26 13:59:33 +0100

    Create function to display board

--- script.js
+++ script.js
@@ -10,13 +10,14 @@ function createPlayer (name, marker) {
 
 const gameBoard = (() => {
 
-    const playerOne;
-    const playerTwo;
+    const board = [["","",""], ["","",""], ["","",""]]
+    let playerOne;
+    let playerTwo;
 
     const startGame = (nameOne, nameTwo) => {
         playerOne = createPlayer(nameOne, "O");
         playerTwo = createPlayer(nameTwo, "X");
-        displayBoard();
+        displayBoard(nameOne, nameTwo);
     }
 
     const checkWinCondition = (player) => {
@@ -26,8 +27,28 @@ const gameBoard = (() => {
 })();
 
 const displayController = (() => {
-    const displayBoard = () => {
 
+    const board = [];
+    const gameContainer = document.querySelector('.gameContainer');
+
+    const createBoard = () => {
+        let boardContainer = document.createElement('div');
+        boardContainer.classList.add("boardContainer");
+        for(let i=0; i<3; i++){
+            let row = [];
+            for(let j=0; j<3; j++){
+                let field = document.createElement('div');
+                field.id = `${i+1}${j+1}`;
+                field.classList.add("field");
+                boardContainer.appendChild(field);
+                row.push(field);
+            }
+            board.push(row);
+        }
+        gameContainer.appendChild(boardContainer);
+    }
+    const displayBoard = (nameOne, nameTwo) => {
+        let boardContainer = createBoard();
     }
     const displayMarker = (field, player) => {
 
@@ -35,6 +56,6 @@ const displayController = (() => {
     const clearBoard = () => {
 
     }
-    return(displayBoard, displayMarker, clearBoard)
+    return {createBoard, displayBoard, displayMarker, clearBoard};
 })();
 
